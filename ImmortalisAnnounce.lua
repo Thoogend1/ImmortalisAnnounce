39088edd35dd21aa15ee57c0dba7751d85a0fc0a
Timo Hoogendorp <timo.hoogendorp@altares.com>
2026-04-17 11:12:38 +0200

    first changes for midnight compatibility

--- ImmortalisAnnounce.lua
+++ ImmortalisAnnounce.lua
@@ -1,4 +1,4 @@
--------IA v11---------
+-------IA v12---------
 -------------------------
 -- OutputChannelConfig --
 ---- msgType = "SAY"; ---
@@ -6,8 +6,6 @@
 -------------------------
 
 local UnitGUID = UnitGUID;
-local GetNumRaidMembers = GetNumRaidMembers;
-local GetNumPartyMembers = GetNumPartyMembers;
 local IsInInstance = IsInInstance;
 local InstanceType = "none"
 
@@ -34,43 +32,45 @@ local function GetRaidIcon(unitFlags)
 	return "{rt"..RaidIconMaskToIndex[raidTarget].."}";
 end
 
-local interr = CreateFrame("Frame", "InterruptTrackerFrame", UIParent);
+local interr = CreateFrame("Frame");
 interr:RegisterEvent("COMBAT_LOG_EVENT_UNFILTERED");
 interr:RegisterEvent("PLAYER_ENTERING_WORLD");
 interr:SetScript("OnEvent", function(self, event, ...)
     if (event == "COMBAT_LOG_EVENT_UNFILTERED") then
-        local type, _, sourceGUID, sourceName, _, _, destGUID, destName, _, destRaidFlags, spellId = select(2, CombatLogGetCurrentEventInfo());
+        local _, type, _, sourceGUID, sourceName, _, _, _, destName, _, destRaidFlags, spellId, _, _, extraSpellID = CombatLogGetCurrentEventInfo();
         if (type == "SPELL_INTERRUPT" and UnitGUID("player") == sourceGUID) then
-            local extraSpellID = select(15, CombatLogGetCurrentEventInfo());
             local destIcon = "";
             if (destName) then
                 destIcon = GetRaidIcon(destRaidFlags);
             end
 
-            local interruptingSpell = C_Spell.GetSpellLink(spellId);
-            local interruptedSpell = C_Spell.GetSpellLink(extraSpellID);
+            local interruptingSpell = C_Spell.GetSpellLink(spellId) or "unknown spell";
+            local interruptedSpell = C_Spell.GetSpellLink(extraSpellID) or "unknown spell";
             local msg = "";
 			if (IsInGroup()) then
                 msg = interruptingSpell.." interrupted "..destIcon..destName.."'s "..interruptedSpell.."!";
             else
-                local destStr = format(TEXT_MODE_A_STRING_SOURCE_UNIT, "", destGUID, destName, destName); -- empty icon, destRaidFlags = 0 when solo
-                msg = "\124cffff4809"..sourceName..": \124r"..interruptingSpell.." \124cffff4809has interrupted "..destStr.."'s\124r "..interruptedSpell.."\124cffff4809!\124r";
+                msg = "\124cffff4809"..sourceName..": \124r"..interruptingSpell.." \124cffff4809has interrupted "..destName.."'s\124r "..interruptedSpell.."\124cffff4809!\124r";
             end
 
             if (GetNumGroupMembers() > 0) then
+                local msgType = "PARTY";
                 if (IsInGroup(LE_PARTY_CATEGORY_INSTANCE) or IsInRaid(LE_PARTY_CATEGORY_INSTANCE)) then
-                    if (InstanceType == "none")then
+                    if (InstanceType == "none") then
                         msgType = "PARTY";
-                    elseif (InstanceType == "party" or InstanceType == "raid" or InstanceType == "scenario" or InstanceType == "delve" or InstanceType == "follower dungeon" or InstanceType == "story raid") then -- Dungeon/Raid (Finder)/Scenario. 
-					-- You can add InstanceType == "pvp" to include battlegrounds
-                    msgType = "INSTANCE_CHAT"; 
+                    elseif (InstanceType == "party" or InstanceType == "raid" or InstanceType == "scenario" or InstanceType == "delve" or InstanceType == "follower dungeon" or InstanceType == "story raid") then -- Dungeon/Raid (Finder)/Scenario.
+                        -- You can add InstanceType == "pvp" to include battlegrounds
+                        msgType = "INSTANCE_CHAT";
                     elseif (IsInRaid(LE_PARTY_CATEGORY_HOME)) then
-                    msgType = "RAID";
+                        msgType = "RAID";
                     else
-                    msgType = "PARTY";
+                        msgType = "PARTY";
                     end
-                end    
-                SendChatMessage(msg, msgType); -- exception. Should logically never happen.
+                end
+                -- Defer to next frame tick to avoid taint from COMBAT_LOG_EVENT_UNFILTERED
+                C_Timer.After(0, function()
+                    SendChatMessage(msg, msgType);
+                end);
             else
                 DEFAULT_CHAT_FRAME:AddMessage(msg);
             end
